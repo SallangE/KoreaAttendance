781eb0af1958dc4a711a95c067147d75bf012ddc
yunseong <ttttma2@naver.com>
2025-02-25 23:43:05 +0900

    Updated database connection settings

--- src/main/java/com/korea/attendance/controller/TestController.java
+++ src/main/java/com/korea/attendance/controller/TestController.java
@@ -1,5 +1,14 @@
 package com.korea.attendance.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.korea.attendance.service.TestService;
+
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
--- src/main/java/com/korea/attendance/service/TestService.java
+++ src/main/java/com/korea/attendance/service/TestService.java
@@ -1,5 +1,10 @@
 package com.korea.attendance.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.korea.attendance.repository.TestMapper;
+
 @Service
 public class TestService {
 
