30da4d6b9cea054f069e6fb8109eff91c21653fd
SallangE <ttttma2@naver.com>
2025-02-26 11:10:12 +0900

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
       <footer className="footer">
         <p>© 2025 Learn-Alive. created by 이현승. All rights reserved.</p>
-        <p>문의: contact@learn-alive.com</p>
+        <p>문의: 2024010085@korea.ac.kr</p>
       </footer>
     );
   };
