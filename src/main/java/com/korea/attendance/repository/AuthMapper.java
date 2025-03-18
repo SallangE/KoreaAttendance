6a921f56a6b363954edea9662d426cf373e8f9dd
SallangE <ttttma2@naver.com>
2025-03-18 19:40:37 +0900

    Update AuthMapper.java

--- src/main/java/com/korea/attendance/repository/AuthMapper.java
+++ src/main/java/com/korea/attendance/repository/AuthMapper.java
@@ -11,13 +11,14 @@ import com.korea.attendance.model.User;
 
 @Mapper
 public interface AuthMapper {
-   @Select("SELECT student_id AS userId, name, 'student' AS role FROM Student WHERE student_id = #{userId} LIMIT 1")
+	@Select("SELECT student_id AS userId, name, 'student' AS role FROM Student WHERE student_id = #{userId} LIMIT 1")
 	@Results({
 	    @Result(column = "userId", property = "userId"),
 	    @Result(column = "name", property = "name"),
 	    @Result(column = "role", property = "role") // ✅ role을 명확하게 매핑
 	})
 	Optional<User> findUserById(String userId);
+
     
  // ✅ 교수자 로그인 시 비밀번호 검증 (단일 조회)
     @Select("SELECT prof_id AS userId, name, 'professor' AS role, password FROM Professor WHERE prof_id = #{userId}")
